bfe0e5f41dca5e84236b686c6d4025dacfbb4dee
Caleb Hargraves <jmjoyh00@gmail.com>
2018-04-26 07:12:47 -0700

    Python Zoo Part 1 Finished
    Django is going strong

--- pythonzoo/zoo/admin.py
+++ pythonzoo/zoo/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Zoo, Exhibit, Neighbor, Animal
+
+class ZooAdmin(admin.ModelAdmin):
+    list_display = ('name', 'id')
+
+admin.site.register(Zoo, ZooAdmin)
+
+class ExhibitAdmin(admin.ModelAdmin):
+    list_display = ('name', 'id')
+
+admin.site.register(Exhibit, ExhibitAdmin)
+
+class NeighborAdmin(admin.ModelAdmin):
+    list_display = ('fromExhibit', 'direction', 'toExhibit')
+
+admin.site.register(Neighbor, NeighborAdmin)
+
+class AnimalAdmin(admin.ModelAdmin):
+    list_display = ('name', 'image', 'sound', 'food', 'political')
+
+admin.site.register(Animal, AnimalAdmin)
--- pythonzoo/zoo/models.py
+++ pythonzoo/zoo/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
 # Create your models here.
+
+from django.urls import reverse
+
+class Zoo(models.Model):
+    name = models.CharField(max_length=200, help_text="Enter a Zoo name.")
+
+    def __str__(self):
+        return self.name
+
+class Exhibit(models.Model):
+    name = models.CharField(max_length=200, help_text="Enter an Exhibit name.")
+    zoo = models.ForeignKey('Zoo', on_delete=models.SET_NULL, null=True)
+    hasExit = models.BooleanField(default=False)
+    animal = models.ForeignKey('Animal', on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Neighbor(models.Model):
+    fromExhibit = models.ForeignKey('Exhibit', on_delete=models.SET_NULL, null=True)
+    direction = models.CharField(max_length=200)
+    toExhibit = models.ForeignKey('Exhibit', on_delete=models.SET_NULL, null=True, related_name='neighbor_of')
+
+    def __str__(self):
+        return str(self.fromExhibit) + ': ' + str(self.direction) + ' to: ' + str(self.toExhibit)
+
+class Animal(models.Model):
+    name = models.CharField(max_length=200)
+    image = models.FileField()
+    sound = models.CharField(max_length=200)
+    food = models.CharField(max_length=200)
+    political = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
